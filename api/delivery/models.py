31278279f66c498c4ce40570a570e1ade171e6d9
Juay En <juayen.ho.2020@scis.smu.edu.sg>
2024-03-29 13:44:17 +0800

    Update models.py

--- api/delivery/models.py
+++ api/delivery/models.py
@@ -5,3 +5,7 @@ from datetime import date
 class Delivery(BaseModel):
     delivery_date: date
     delivery_map: Dict[str, List[str]]
+
+class ClientDelivery(BaseModel):
+    delivery_date: date
+    delivery_id: str
