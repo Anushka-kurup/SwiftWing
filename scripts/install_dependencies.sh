2ab4c0a6498c3b6450adf0c90cb5ae64cde69be8
Ho-JE <juayen.ho.2020@scis.smu.edu.sg>
2024-03-31 20:26:08 +0800

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t swiftwing-fe:1.0 -f /SwiftWing/Ui-devias/Dockerfile .
+docker build -t swiftwing-fe:1.0 -f ../Ui-devias/Dockerfile .
 
-docker build -t swiftwing-be:1.0 -f /SwiftWing/api/Dockerfile .
+docker build -t swiftwing-be:1.0 -f ../api/Dockerfile .
